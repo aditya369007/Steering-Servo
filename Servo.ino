0897fb18a4d22df43319d4e0a16675bc1a5c94fa
Aditya Mulampally <aditya.mulampally@gmail.com>
2017-10-11 17:33:28 -0500

    Add files via upload
    Base code. A= left D= right S=stop.
    Using Delays.

---
+++ Servo.ino
@@ -0,0 +1,57 @@
+#include "TimerOne.h"
+
+
+
+const int Enable = 6;
+const int InA = 7;
+const int InB = 11;
+unsigned long starttime;
+unsigned long endtime;
+byte incdata;
+void setup() {
+  pinMode(10,OUTPUT);
+  Timer1.initialize();
+
+  pinMode(Enable, OUTPUT);
+  pinMode(InA, OUTPUT);
+  Serial.begin(9600);
+  
+  // put your setup code here, to run once:
+ digitalWrite(Enable, HIGH);
+ 
+}
+
+void loop() {
+
+
+ if (Serial.available()>0)
+ {
+  incdata = Serial.read();
+
+  if (incdata == 'd')
+  {
+     digitalWrite(InA, LOW);
+analogWrite(InB, 1);
+delay(1500*8);
+analogWrite(InB,0);
+    }
+    
+   if (incdata == 'a')
+  {
+    
+    
+    digitalWrite(InA, LOW);
+analogWrite(InB, 254);
+delay(1500*8);
+
+}
+    }
+  if (incdata == 's')  
+ {
+  digitalWrite(InA,HIGH); 
+  }
+
+  
+  } 
+
+}
